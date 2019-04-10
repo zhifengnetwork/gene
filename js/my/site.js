e0a56c20909516b06de9d3f1c0fcf00a64cfc95e
ICEY-I-SEE <1141839113@qq.com>
2019-04-10 15:14:50 +0800

    :art:修改地址样式

--- js/my/site.js
+++ js/my/site.js
@@ -3,10 +3,10 @@ $(function(){
 				$(".default").click(function(){
 					if($(this).hasClass('active')){
 						$(this).removeClass('active')						
-						$(this).find('img').attr('src','../../img/home/Button-box@2x.png');
+						$(this).find('img').attr('src','../../img/purchase/Button-box@2x.png');
 					}else{
 						$(this).addClass('active');
-						$(this).find('img').attr('src','../../img/home/button@2x.png');
+						$(this).find('img').attr('src','../../img/purchase/button@2x.png');
 					}
 				})
 				//点击保存按钮获取页面数据
