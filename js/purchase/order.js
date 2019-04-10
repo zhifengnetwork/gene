19a9d004db0684d4d6e0fd860b96c785e81487ff
16539693705 <1659693705@qq.com>
2019-04-10 10:54:21 +0800

    订单

--- js/purchase/order.js
+++ js/purchase/order.js
@@ -1,6 +1,8 @@
 // 订单确认
 $(function(){
-    
+     //页面加载调用函数求总价
+     sumTotal()
+
      //使用余额
     $('.difference').click(function(){
         var acc = $(this)
@@ -17,19 +19,47 @@ $(function(){
 
     //加
     $('.add').click(function(){
+       var $sub = $(this)
        var num = $(this).prev().prop('value')
        var s = parseInt(num)+1
        if(s == 0){return;}
-       $(this).prev().attr('value',s)
+       $sub.prev().attr('value',s)
+       //单行数量
+       $sub.parent().parent().prev().children(':last-child').children('.num').html(`x${s}`)
+    //    sumPrice($sub.next(),parseInt($('.price').html().slice(1)))
+       //调用求和
+       sumTotal()
     })
 
     //减
     $('.subtract').click(function(){
+        var $sub = $(this)
         var num = $(this).next().prop('value')
         var s = parseInt(num)-1
         if(s == 0){return;}
-        $(this).next().attr('value',s)
+        $sub.next().attr('value',s)
+        //单行数量
+        $sub.parent().parent().prev().children(':last-child').children('.num').html(`x${s}`)
+        // sumPrice($sub.next(),parseInt($('.price').html().slice(1)))
+        //调用求和
+       sumTotal()
     })
 
+    //总价求和函数封装
+    function sumTotal(){
+        var total=0;
+        $(".price").each((i,elem)=>{
+            total+=parseInt($(elem).html().slice(1))*parseInt($(elem).next().html().slice(1))
+        })
+        $(".price_red").html(`¥${total.toFixed(2)}`)
+    }
+
+        //单行小计求和函数封装
+    // function sumPrice(numObject,priceObject){
+    //     // var price=priceObject.parent().prev().html().replace(/\s+$|^\s+/g,"").slice(1);
+    //     var price=priceObject
+    //     var num=numObject.attr('value')
+    //     return price*num
+    // }
     
 })
