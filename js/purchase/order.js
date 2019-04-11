f15cf1d6a31efde533b47a0169cde0928d97e376
16539693705 <1659693705@qq.com>
2019-04-11 14:20:01 +0800

    优惠券

--- js/purchase/order.js
+++ js/purchase/order.js
@@ -84,6 +84,65 @@ $(function(){
             }
     })
 
-    
+    //领取优惠券
+    $('.mode-yhq').click(function(){
+        var add = $(this)
+        $('.receive_coupon').show()  //遮罩层
+        $('.receive_box').show()     //优惠券
+        thisScrollNum = $(document).scrollTop();
+				$('.wrap_frame').css({
+					'position': 'fixed',
+					'top': -thisScrollNum,
+					'left': 0,
+                    'height': '100%',
+                    'width':'100%',
+                    'touch-action':'pan-y'
+				});
+				$('html').css({
+					'height': '100%'
+				});
+    })
+
+    //隐藏优惠券
+    $('.receive_coupon').click(coupon) //遮罩层
+    $('.achieve').click(coupon)        //完成
+
+
+    function coupon(){
+        $('.receive_coupon').hide()  //遮罩层
+        $('.receive_box').hide()     //优惠券
+         /*恢复滑动*/
+         $('.wrap_frame').css({
+            'position': '',
+            'top': '',
+            'left': '',
+            'height': '',
+            'width':'',
+            'touch-action':''
+        });
+        $('html').css({
+            'height': ''
+        });
+        /*恢复当前用户滚动的位置！*/
+        $(document).scrollTop(thisScrollNum);
+		$("receive_coupon,body").unbind("touchmove");
+    }
+
+    //使用优惠券
+    $('.use').click(function(){
+         var employ = $(this)
+         var img = `<img src="../../img/purchase/logo@2x.png" alt="" class="been">`
+         if(employ.parent().hasClass('coupon_use')){
+         employ.parent().addClass('employ').removeClass('coupon_use')
+         employ.parent().append(img)
+         $('.popup').show()
+         $('.popup').animate({
+             opactiy:1
+         }
+         ,1000,function(){
+            $('.popup').hide()
+         }) 
+         }
+    })
 
 })
