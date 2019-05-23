160a455db35ec888efc2130777bcac3ef5dbb0b4
hlb243X <2726724651@qq.com>
2019-05-23 14:42:12 +0800

    “合并代码

--- js/public/public.js
+++ js/public/public.js
@@ -1,5 +1,10 @@
+/*只能点一次，'返回',防止重复点击·*/
+var state = true;
 /*返回 按钮*/
 function returnFun(){
+	if(!state){
+		return false;
+	}
 	/*返回上一页*/
 	if($('.lb_headWrap .lb_headWrap_return').attr('data-num') == 1 || $('.headWrap_lb .returnBut_lb').attr('data-num') == undefined ){
 		window.history.back();
@@ -8,6 +13,7 @@ function returnFun(){
 		/*页面跳转*/
 		window.location.href = $('.lb_headWrap .lb_headWrap_return').attr('data-num');
 	}
+	state = false;
 	return false;
 }
 
